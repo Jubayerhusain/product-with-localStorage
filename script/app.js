2061ec1b8cc318db0f9f31e78d936615ab3dc606
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-14 13:32:00 +0600

    local storage done

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-console.log('Your js fine doen to added')
+// console.log('Your js fine doen to added')
 
 const getInputField = () => {
     const productInput = document.getElementById('product-input');
@@ -7,17 +7,46 @@ const getInputField = () => {
     const quantity = productQuantity.value;
     productInput.value = '';
     productQuantity.value = '';
-    console.log(product, quantity)
-    displayInputValue(product, quantity)
-    
+    // console.log(product, quantity);
+    displayInputValue(product, quantity);
+    saveListLocalStorage(product, quantity);
+
 }
 const displayInputValue = (product, quantity) => {
     const products = product;
     const quantitys = quantity;
     const productContainer = document.getElementById('product-container');
     const li = document.createElement('li');
-    li.innerText=`
+    li.innerText = `
      ${products}: ${quantitys}
     `;
     productContainer.appendChild(li);
 }
+
+// get the product from localStorage (if) it has alredy stored the localStorage and (else) return the empty object..
+
+const getStoredlist = () => {
+    let list = {};
+    const storedList = localStorage.getItem('list');
+    if (storedList) {
+        list = JSON.parse(storedList)
+    }
+    return list;
+}
+// getStoredlist();
+const saveListLocalStorage = (product, quantity) => {
+    const list = getStoredlist();
+    list[product] = quantity;
+    const listSting = JSON.stringify(list);
+    localStorage.setItem('list', listSting);
+}
+
+const getTheProductFromLocalstorage = () => {
+    const savedList = getStoredlist();
+    for (const product in savedList) {
+        const quantity = savedList[product];
+        displayInputValue(product, quantity);
+
+    }
+}
+getTheProductFromLocalstorage();
