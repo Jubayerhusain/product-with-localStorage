dc078ea64e1a23da815099a3137478255dee9890
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-14 12:37:59 +0600

    DOM done

--- script/app.js
+++ script/app.js
@@ -1,1 +1,23 @@
 console.log('Your js fine doen to added')
+
+const getInputField = () => {
+    const productInput = document.getElementById('product-input');
+    const productQuantity = document.getElementById('quantity-input');
+    const product = productInput.value;
+    const quantity = productQuantity.value;
+    productInput.value = '';
+    productQuantity.value = '';
+    console.log(product, quantity)
+    displayInputValue(product, quantity)
+    
+}
+const displayInputValue = (product, quantity) => {
+    const products = product;
+    const quantitys = quantity;
+    const productContainer = document.getElementById('product-container');
+    const li = document.createElement('li');
+    li.innerText=`
+     ${products}: ${quantitys}
+    `;
+    productContainer.appendChild(li);
+}
